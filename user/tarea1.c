41ae8c114b4c95213da4f589adcf2be682825ccc
Th3Extr3mist <146364367+Th3Extr3mist@users.noreply.github.com>
2024-11-24 16:51:50 -0300

    tarea 1 parte 2

--- user/tarea1.c
+++ user/tarea1.c
@@ -4,6 +4,22 @@
 int
 main(void) {
     int ppid = getppid();
-    printf("El PID del proceso padre es: %d\n", ppid);
+    printf("El PPID del proceso padre es: %d\n", ppid);
+
+    int pid = getpid();
+    printf("PID actual: %d\n", pid);
+
+    int ancestro1 = getancestor(0);
+    printf("Ancestro 0 (hijo): %d\n", ancestro1);
+
+    int ancestro2 = getancestor(1);
+    printf("Ancestro 1 (padre): %d\n", ancestro2);
+
+    int ancestro3 = getancestor(2);
+    printf("Ancestro 2 (abuelo): %d\n", ancestro3);
+
+    int invalido = getancestor(10);
+    printf("Ancestro inválido: %d\n", invalido);
+
     exit(0);
 }
